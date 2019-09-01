d46bd940e1492431fd4c3ef4118a3aafcfda7868
Casey Brooks <cjbrooks12@gmail.com>
2019-09-01 13:46:22 -0500

    Sets up usage of `asset` filter, moves most stylesheets/scripts to Theme class

--- theme/src/main/java/com/github/skanjo/universal/UniversalTheme.java
+++ theme/src/main/java/com/github/skanjo/universal/UniversalTheme.java
@@ -15,6 +15,30 @@ public class UniversalTheme extends Theme {
     @Override
     protected void loadAssets() {
         super.loadAssets();
+
+        // You can keep the vendor assets in this repo, or you can use an HTTP url to a CDN. When serving locally,
+        // Orchid will use the CDN URL to keep builds fast, but when publishing the site, Orchid will download those
+        // assets automatically and reference the local file, to be sure your site still works even if that CDN
+        // goes down.
+        addJs("assets/js/vendor/jquery/jquery.min.js");
+        addJs("assets/js/vendor/popper.js/umd/popper.min.js");
+        addJs("assets/js/vendor/bootstrap/js/bootstrap.min.js");
+        addJs("assets/js/vendor/jquery.cookie/jquery.cookie.js");
+        addJs("assets/js/vendor/waypoints/lib/jquery.waypoints.min.js");
+        addJs("assets/js/vendor/jquery.counterup/jquery.counterup.min.js");
+        addJs("assets/js/vendor/owl.carousel/owl.carousel.min.js");
+        addJs("assets/js/vendor/owl.carousel2.thumbs/owl.carousel2.thumbs.min.js");
+        addJs("assets/js/jquery.parallax-1.1.3.js");
+        addJs("assets/js/vendor/bootstrap-select/js/bootstrap-select.min.js");
+        addJs("assets/js/vendor/jquery.scrollto/jquery.scrollTo.min.js");
+        addJs("assets/js/front.js");
+
+        addCss("assets/js/vendor/bootstrap/css/bootstrap.min.css");
+        addCss("assets/js/vendor/bootstrap-select/css/bootstrap-select.min.css");
+        addCss("assets/js/vendor/owl.carousel/assets/owl.carousel.css");
+        addCss("assets/js/vendor/owl.carousel/assets/owl.theme.default.css");
+        addCss("assets/css/style.default.css");
+        addCss("assets/css/custom.css");
     }
 
 }
